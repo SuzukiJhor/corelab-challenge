e1561776beb6de073131e45a4e2ce867cfbb8f20
JhordanKS <jhordan.kendy1@gmail.com>
2024-03-30 04:12:32 -0300

    feat: cerate validated for tasks.

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\DTO\Tasks\CreateTaskDTO;
+use App\DTO\Tasks\EditTaskDTO;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Repositories\Eloquent\TaskRepository;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Models\Task;
 
 class TaskController extends Controller
@@ -24,23 +29,33 @@ class TaskController extends Controller
         return TaskResource::collection($tasks);
     }
 
-    public function store(Request $request)
+    public function store(StoreTaskRequest $request)
     {
-        //
+        $task = $this->taskRepository->create(new CreateTaskDTO(...$request->validated()));
+        return new TaskResource($task);
     }
 
     public function show(string $id)
     {
-        //
+        if (!$task = $this->taskRepository->findById($id)) {
+            return response()->json(['message' => 'task not found'], Response::HTTP_NOT_FOUND);
+        }
     }
 
-    public function update(Request $request, string $id)
+    public function update(UpdateTaskRequest $request, string $id)
     {
-        //
+        $response = $this->taskRepository->update(new EditTaskDTO(...[$id, ...$request->validated()]));
+        if (!$response) {
+            return response()->json(['message' => 'task not found'], Response::HTTP_NOT_FOUND);
+        }
+        return response()->json(['message' => 'task updated with success']);
     }
 
     public function destroy(string $id)
     {
-        //
+        if (!$this->taskRepository->delete($id)) {
+            return response()->json(['message' => 'task not found'], Response::HTTP_NOT_FOUND);
+        }
+        return response()->json(['message' => 'task deleted with success'], Response::HTTP_NO_CONTENT);
     }
 }
---
+++ backend/app/Http/Requests/UpdateTaskRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'min:2', 'max:255', 'unique:tasks'],
+            'description' => ['nullable', 'string', 'min:2'],
+            'is_favorite' => ['in:true,false'],
+            'color' => ['string', 'max:255'],
+        ];
+    }
+}
