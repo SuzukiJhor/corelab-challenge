e7cada6975149fd52e212373878cf3a9489905f0
JhordanKS <jhordan.kendy1@gmail.com>
2024-04-01 01:55:39 -0300

    feat: create component Dashboard

---
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+const DashboardContainer = styled.div`
+  background: #f0f2f5;
+  height: 100vh;
+`;
+
+const Dashboard = () => {
+  return (
+    <DashboardContainer>
+      <h1>Dashboard</h1>
+    </DashboardContainer>
+  );
+};
+
+export default Dashboard;
