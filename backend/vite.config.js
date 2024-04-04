0deebccd27baab66ec6367f79c544d286fbbf82b
Jhordan S <95131108+SuzukiJhor@users.noreply.github.com>
2024-04-04 20:27:04 -0300

    fix: update vite.config.js

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  plugins: [react()],
 });
